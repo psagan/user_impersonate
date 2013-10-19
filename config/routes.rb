d27c133b967beef79ef518afd480c932fd89ecbc
Patrick Sagan <psagan@icloud.com>
2013-10-19 21:02:39 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 UserImpersonate::Engine.routes.draw do
-  match("/user/:user_id" , to: "impersonate#create", as: :impersonate_user)
-  match("/revert" , to: "impersonate#destroy", as: :revert_impersonate_user)
+  match("/user/:user_id" , to: "impersonate#create", as: :impersonate_user, via: [:get])
+  match("/revert" , to: "impersonate#destroy", as: :revert_impersonate_user, via: [:get])
   
   root to: "impersonate#index"
 end
